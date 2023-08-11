f2e05455b4f7286f4c52d97c2e2b4cef3eed8329
alykaa <alykaasalaah@Alykaas-MBP.home>
2023-08-10 21:55:34 -0400

    new push

---
+++ app.js
@@ -0,0 +1,35 @@
+// Declare ten different variables
+var a = 5;
+var b = 10;
+var c = 15;
+var d = 20;
+var e = 25;
+var f = 30;
+var g = 35;
+var h = 40;
+var i = 45;
+var j = 50;
+
+// 2x console.log() addition of two variables
+console.log("a + g = ", a + g);
+console.log("c + j = ", c + j);
+
+// 2x console.log() subtraction of two variables
+console.log("f - e = ", f - e);
+console.log("b - h = ", b - h);
+
+// 2x console.log() multiplication of two variables
+console.log("h * d = ", h * d);
+console.log("i * f = ", i * f);
+
+// 2x console.log() division of two variables
+console.log("e / c = ", e / c);
+console.log("g / h = ", g / h);
+
+// 2x console.log() incerments of two variables
+console.log("f * 2 ", f += 2);
+console.log("j * 2 ", j += 2);
+
+// 2x console.log() decrement of two variables
+console.log("a - 2 ", a -= 2);
+console.log("b - 2 ", b -= 2);
